99ba365bb0918318ff7cec564075649194e31519
bugcrowd1103 <bugcrowd1103@gmail.com>
2025-12-30 23:45:25 -0500

    chore: hardcode keys for urgency

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = "https://byviaowigbgthpnxculn.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5dmlhb3dpZ2JndGhwbnhjdWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2MTA4MTQsImV4cCI6MjA4MjE4NjgxNH0.2j8qe8knk_w3PbrUl2RqH6gwSByJZ4IwQ5NVl3QtI0c";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
